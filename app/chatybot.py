d29ab1f2f2f0fff30690640a4801b104ba55b67d
devVincii <devvinci7@gmail.com>
2026-01-11 12:06:36 +0200

    improved modelfix

--- app/chatybot.py
+++ app/chatybot.py
@@ -135,6 +135,8 @@ class TextProcessor:
         # Convert the lists to NumPy arrays for training
         self.X = np.array(bags)
         self.y = np.array(indices)
+        
+        
 ###################################################################################################################
     def training_model(self, input_size, output_size, hidden_size=8, num_epochs=1000, batch_size=8, learning_rate=0.001):
         # Convert training data from NumPy arrays to PyTorch tensors
